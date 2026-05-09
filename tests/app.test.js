eefd4a77f58e0052669688367b281d5c65d252c4
Suraj Prakash <surajprak201@gmail.com>
2026-05-09 13:00:35 +0530

    dd

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,31 +1,22 @@
 const request = require('supertest');
 const app = require('../index');
 
-describe('Test the API', () => {
-
-    test('Test GET method API', async () => {
-
+describe('test the api', () => {
+    
+    test('test get method api', async () => {
         const res = await request(app).get('/');
-
         expect(res.statusCode).toBe(200);
-
         expect(res.text.replace(/\s+/g, " ").trim()).toBe(
-            " <h1>Welcome to the app</h1>
-            < h2 > Name: Rohit Kumar</ > "
+            "<h1>Welcome to the app</h1> <h2>Name: Rohit Kumar</h2>"
         );
     });
 
-    test('Test POST method API', async () => {
-
+    test('test post method api', async () => {
         const res = await request(app)
             .post('/add')
-            .send({
-                num1: 10,
-                num2: 15
-            });
+            .send({ num1: 10, num2: 15 });
 
         expect(res.statusCode).toBe(200);
-
         expect(res.body.result).toBe(25);
     });
 
